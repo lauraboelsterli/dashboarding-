c68120cd46239b467db0737baa0578b6bce9b5f0
Laura B <boelsterlilaura@gmail.com>
2024-10-14 18:24:35 -0400

    updated docstrings and did some cleaning to my dashbaord file

--- laura_dashboard.py
+++ laura_dashboard.py
@@ -1,30 +1,21 @@
 import panel as pn
 from laura_etf_api import etf_API
-# import sankey as sk
 import datetime as dt
-
-# import panel as pn
 import pandas as pd
-import altair as alt
 import plotly.graph_objects as go
-from matplotlib.figure import Figure
-# import hvplot.pandas
 import time_series as ts
 
 # Loads javascript dependencies and configures Panel (required)
 pn.extension()
 
-
 # INITIALIZE API
 api = etf_API()
 api.load_df('data/ETFprices.csv')
-
 
 # WIDGET DECLARATIONS
 # Search Widgets
 fund_name = pn.widgets.MultiSelect(name="Select All ETFs of Interest", options=api.get_funds(), value=['SPY', 'QQQ', 'GLD'], 
     height=250)
-# fund_name = pn.widgets.Select(name="Fund", options=api.get_funds(), value='AAA')
 timeseries_filter = pn.widgets.Select(name="Market Price Data", options = api.get_options(), value='close')
 date_range_slider = pn.widgets.DateRangeSlider(
     name='Select a Date Range',
@@ -37,82 +28,69 @@ ts_width = pn.widgets.IntSlider(name="Width", start=250, end=800, step=50, value
 ts_height = pn.widgets.IntSlider(name="Height", start=200, end=800, step=50, value=400)
 trend_width = pn.widgets.IntSlider(name="Width", start=50, end=600, step=50, value=300)
 trend_height = pn.widgets.IntSlider(name="Height", start=50, end=600, step=50, value=100)
-# vol_width = pn.widgets.IntSlider(name="Width", start=200, end=400, step=50, value=300)
-# vol_height = pn.widgets.IntSlider(name="Height", start=200, end=400, step=50, value=300)
 
-# this decorator tells the function when to rerun 
+
+
+# this decorator tells the function when to rerun
+# aka everytime fund_name chosen from the multiselect
 @pn.depends(fund_name.param.value, watch=True)
 def update_date_range(fund_name):
-    '''updates the time range slider depending on the 
-    minimum date and maximum date funds for each given 
-    fund seleciton'''
-    # selected_funds = event.new
+    '''-laura 
+    params: fund name (lst or str)
+    does: eerytime new fund_names are chosen on the dashboard this function gets triggered, to dynamically 
+    change the date range slider options to only show relevant dates for those given funds (returns
+    minimum date and maximum dates from given fund(s) seleciton)
+    functionality and implementation found here: https://panel.holoviz.org/api/panel.depends.html
+    returns: updated slider widget on the dashboard
+    '''
+
     min_date = api.fund_df[api.fund_df['fund_symbol'].isin(fund_name)]['price_date'].min()
     max_date = api.fund_df[api.fund_df['fund_symbol'].isin(fund_name)]['price_date'].max()
     date_range_slider.start = min_date
     date_range_slider.end = max_date
     date_range_slider.value = (min_date, max_date)
 
-# def update_date_range(event):
-#     selected_funds = event.new
-#     if selected_funds:
-#         min_date = api.fund_df[api.fund_df['fund_name'].isin(selected_funds)]['price_date'].min()
-#         max_date = api.fund_df[api.fund_df['fund_name'].isin(selected_funds)]['price_date'].max()
-#         date_range_slider.start = min_date
-#         date_range_slider.end = max_date
-#         date_range_slider.value = (min_date, max_date)
-# fund_name.param.watch(update_date_range, 'value')
-
-
 
 # CALLBACK FUNCTIONS
 
-# time slider 
 def get_plotly(fund_name, timeseries_filter, date_range_slider, width, height):
-    '''given fundname, value of itnerest (e.g. open,close prices), 
-    returns a time series plot for one or more etf funds '''
+    '''-laura
+    params: fund_name (name of fund(s)(list or str)), timeseries_filter (market value of interest (str)), 
+    date_range_slider (start and end date (tuple), width (int), height (int)
+    does: given a fund name(s), a value of interest (e.g. open,close prices), and time range, a time series plot 
+    is plotted on plotly 
+    returns: a time series figure for one or more etf funds
+    '''
     # global filtered_local 
     filtered_local = api.get_filtered_data(fund_name, timeseries_filter, date_range_slider)
     # plotting time series 
     fig = ts.make_time_series(fund_name, filtered_local, timeseries_filter, width, height)
 
-    # fig = go.Figure()
-
-    # for etf in fund_name:
-    #     etf_data = filtered_local[filtered_local['fund_symbol'] == etf]  # filter data for each ETF
-    #     fig.add_trace(go.Scatter(x=etf_data['price_date'], y=etf_data[timeseries_filter],
-    #         mode='lines', name=etf, hoverinfo='x+y'))
-
-    # # Customize the layout with titles and axis labels
-    # fig.update_layout(
-    #     title="ETF Price Tracker",
-    #     xaxis_title="Date",
-    #     yaxis_title=timeseries_filter,
-    #     legend_title="ETF",
-    #     hovermode="x unified"  # Shows the hover info for all traces at the same x-position
-    # )
-
     return fig
 
 
 def get_trend_indicator(fund_name, timeseries_filter, date_range_slider, width=250, height=200):
-    '''get trends for each fund automatically calculated (y value (chosen value of interest) 
+    '''-laura
+    params: fund_name (name of fund(s)(list or str)), timeseries_filter (market value of interest (str)), 
+    date_range_slider (start and end date (tuple), width (int), height (int)
+    does: get trends for each fund automatically calculated (y value (chosen value of interest) 
     most recent value percentage change is computed by first value (from chosen start date) 
     and last value (from chosen end date)) measures done by indicators widget 
-    and then each fund with its metrics gets returns as column'''
-    # collecting all Trend indicators
+    and then each fund with its metrics gets returns as column
+    returns: trend indicator widgets '''
+    # collecting all trend indicators
     trends = []  
     
     # Iterate over each fund symbol in the list
     for symbol in fund_name:
-        # Use the API function to fetch and filter the data
+        # fetching and filter the data
         df = api.get_filtered_data([symbol], timeseries_filter, date_range_slider)
         # print(df)
 
         # prepping data for the trend indicator in dct format
         data = {'x': df['price_date'].values, 'y': df[timeseries_filter].values}
         
-        # Create the Trend indicator for the ETF
+        # creating the Trend indicator for the ETF
         trend = pn.indicators.Trend(
             name=f'{symbol} Price Trend',
             data=data,
@@ -135,12 +113,17 @@ def get_trend_indicator(fund_name, timeseries_filter, date_range_slider, width=2
 
 
 def get_total_volume_plot(fund_name, date_range_slider, width=600, height=400):
-    '''make bar plots for total volume for each given fund'''
+    '''-laura 
+    params: fund_name (name of fund(s)(list or str)), 
+    date_range_slider (start and end date (tuple), width (int), height (int)
+    does: makes bar plots for total volume for each given fund
+    returns: figure of bar plot made on plotly 
+    '''
     df = api.get_filtered_data(fund_name, 'volume', date_range_slider)
 
-    # Calculate total volume per ETF
+    # calculate total volume per ETF
     total_volumes = df.groupby('fund_symbol')['volume'].sum().reset_index()
-    # Plot total volume
+    # plotting the total volume:
     fig = go.Figure()
     fig.add_trace(go.Bar(
         x=total_volumes['fund_symbol'], 
@@ -160,8 +143,15 @@ def get_total_volume_plot(fund_name, date_range_slider, width=600, height=400):
     
     return fig
 
-def get_total_volume_indicator(fund_name, date_range_slider, width=200, height=50):
-    '''Create a summary indicator for total volume over the selected time range for each ETF with numeric values and suffixes in the name.'''
+def get_total_volume_indicator(fund_name, date_range_slider, width=300, height=300):
+    '''-laura 
+    params: fund_name (name of fund(s)(list or str)), 
+    date_range_slider (start and end date (tuple), width (int), height (int)
+    does: creates a summary indicator for total volume over the selected time range for 
+    each ETF with numeric values formatted with suffixes to indicate if the number 
+    is in billions, millions, etc., to reduce clutter and enhance readability
+    returns: returns volume indicator widgets 
+    '''
     df = api.get_filtered_data(fund_name, 'volume', date_range_slider)
 
     # Calculate total volume per ETF
@@ -195,49 +185,23 @@ def get_total_volume_indicator(fund_name, date_range_slider, width=200, height=5
             height=height
         )
         indicators.append(indicator)
-            # Arrange indicators in a Row and make it horizontally scrollable
-    scrollable_row = pn.Column(
-        pn.Row(*indicators, sizing_mode='stretch_width'),
-        scroll=True,
-        width=1000 # Fixed width; adjust based on how much space you want to display
-        # height=height + 20  # Add some padding to height if needed
-    )
 
-    return scrollable_row
-
-    # return pn.Row(*indicators)
-
-
+    return pn.Row(*indicators)
 
 
 
 
 # CALLBACK BINDINGS (Connecting widgets to callback functions)
-
-
-# time_slider = pn.bind(update_date_range, fund_name)
 plot = pn.bind(get_plotly, fund_name, timeseries_filter, date_range_slider, ts_width, ts_height)
 trend_indicators = pn.bind(get_trend_indicator, fund_name, timeseries_filter, date_range_slider.param.value, trend_width, trend_height)
-# total_volume_plot = pn.bind(get_total_volume_plot, fund_name, date_range_slider.param.value, vol_width, vol_height)
-
-
-total_volume_plot = pn.bind(get_total_volume_indicator, fund_name, date_range_slider.param.value, width=300, height=300)
-# total_volume_plot = pn.bind(get_total_volume_indicator, fund_name, date_range_slider.param.value)
-
-# maybe add average volume traded on the side of the volume plot (similar look to the trend indicators next to the time series plot)
-# also make the width and height of the cards adjustable
-# and have consistent color coding across all plots 
-# strech the bars horizontally below the time series plot 
-
-
+volume_indicators = pn.bind(get_total_volume_indicator, fund_name, date_range_slider.param.value)
 
 
 # DASHBOARD WIDGET CONTAINERS ("CARDS")
 trend_indicators_scrollable = pn.Column(trend_indicators, scroll=True, height=400)  # Set height limit
-
-volume_plotting = pn.Column(total_volume_plot, height=400)
-
-volume_plot = pn.Row(total_volume_plot, scroll=True)
+scrollable_row = pn.Column(pn.Row(volume_indicators),scroll=True,width=1000)
+# volume_plot = pn.Row(total_volume_plot, scroll=True)
+volume_plot = pn.Row(scrollable_row, scroll=True)
 # Combine into a single layout line
 plot_and_trend = pn.Column(pn.Row(plot, trend_indicators_scrollable ), volume_plot)
 
@@ -269,24 +233,15 @@ trend_card = pn.Card(
     collapsed=True
 )
 
-# volume_card = pn.Card(
-#     pn.Column(vol_width, vol_height),
-#     title="Total Volume Dimensions",
-#     width=card_width,
-#     sizing_mode='stretch_width',  
-#     collapsed=True
-# )
-
 stacked_cards = pn.Column(
     search_card,
     plot_card,
     trend_card,
-    # volume_card,
     sizing_mode='stretch_width'
     )
 
-# LAYOUT
 
+# LAYOUT
 layout = pn.template.FastListTemplate(
     title="ETF Explorer",
     sidebar=[
@@ -296,12 +251,6 @@ layout = pn.template.FastListTemplate(
     theme_toggle=False,
     main=[
         plot_and_trend
-        # pn.Tabs(
-            # ("ETF Time Series", plot_and_trend),
-            # ("Volume Traded", volume_plotting),  # Replace None with callback binding
-            # active=0  # Which tab is active by default
-        # )
-
     ],
     header_background='#a93226'
 
